a6e04169b6af1a204e45139a2cc645f94d2f9fbb
Seun Daniel Omatsola <omatsolaseund@gmail.com>
2022-03-30 05:27:24 +0100

    finished

--- SpiritEcommerce/wwwroot/js/site.js
+++ SpiritEcommerce/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.getElementById("more").addEventListener("click", (e) => {
+    document.querySelector(".nav-bar2").style.display = "block";
+    document.getElementById("nav-link-wrapper").style.display = "flex";
+    document.getElementById("more").style.display = "none";
+    document.getElementById("nav-link-wrapper").style.flexDirection = "column";
+});
